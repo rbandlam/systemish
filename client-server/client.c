a2c30ff02d3dc15bca280c7cfe027a849636bc91
root <root@anuj1.softiwarp.fawn.susitna.pdl.cmu.local>
2013-10-19 02:56:53 -0400

    Formatting changes to client-server

--- client-server/client.c
+++ client-server/client.c
@@ -2,17 +2,17 @@
 
 void error(char *msg)
 {
-    perror(msg);
-    exit(0);
+	perror(msg);
+	exit(0);
 }
 
 //Usage: ./client <server_ip>
 int main(int argc, char *argv[])
 {
-    int sockfd;
+	int sockfd;
 
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
 	struct my_struct *send_struct;
 	send_struct = malloc(sizeof(*send_struct));	
@@ -20,25 +20,25 @@ int main(int argc, char *argv[])
 	send_struct->a = 3185;
 	send_struct->b = 3187;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+		error("ERROR opening socket");
 
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
+	server = gethostbyname(argv[1]);
+	if (server == NULL)
 		error("No such host");
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+			(char *)&serv_addr.sin_addr.s_addr,
+			server->h_length);
 	serv_addr.sin_port = htons(STRUCT_PORT);
 
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+		error("ERROR connecting");
 
-	if (write(sockfd,send_struct,sizeof(*send_struct)) < 0)
-		error("ERROR writing to socket");
-    return 0;
+	if (write(sockfd, send_struct, sizeof(*send_struct)) < 0)
+    	error("ERROR writing to socket");
+	return 0;
 }
