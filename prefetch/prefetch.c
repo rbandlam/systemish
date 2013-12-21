e899d768e9dcb2dbaa907245375a6187747d93a2
Anuj Kalia <anujkaliaiitd@gmail.com>
2013-12-21 15:38:35 -0500

    Added working prefetching code. Good performance improvement on susitna

--- prefetch/prefetch.c
+++ prefetch/prefetch.c
@@ -4,47 +4,52 @@
 #include<time.h>
 
 #define CAP (128 * 1024 * 1024)
+#define N (1024 * 1024)
+
+int do_work(int a)
+{
+	int i = 0;
+	for(i = 0; i < 110; i++) {
+		a = a ^ i;
+	}
+	return a;
+}
 int main(int argc, char **argv)
 {
 	int prefetch = atoi(argv[1]);
 	int *A = (int *) malloc(CAP * sizeof(int));
-	int *Ind = (int *) malloc(CAP * sizeof(int));
+	int *Ind = (int *) malloc(N * sizeof(int));
 
 	int i;
 	for(i = 0; i < CAP; i++) {
 		A[i] = i;
+	}
+	for(i = 0; i < N; i++) {
 		Ind[i] = rand() % CAP;
 	}
+
 	printf("Finished assigning data and indices\n");
 
-	int hi = 0, lo = 0, ans = 0;
+	int ans = 0;
 	struct timespec start, end;
 	clock_gettime(CLOCK_REALTIME, &start);
 
 	if(prefetch) {
-		while(1) {
-			//printf("hi = %d, lo = %d\n", hi, lo);
-			if(hi < CAP && hi - lo < 256) {
-				int index = Ind[hi];
-				//printf("\tPrefetching %d\n", hi);
-				__builtin_prefetch(&A[index]);
-				hi ++;
+		for(i = 0; i < N; i ++) {
+			int index = Ind[i];
+			if(index != N - 1) {
+				int next_index = Ind[i + 1];
+				__builtin_prefetch(&A[next_index], 0, 3);
 			}
-			if(hi - lo >= 256 || hi == CAP) {
-				//printf("\tAccessing %d\n", lo);
-				int index = Ind[lo];
-				ans += A[index];
-				lo ++;
-			}
-			if(lo == CAP) {
-				break;
-			}
+			int load = A[index];
+			ans += do_work(load);
 		}
 		printf("ans = %d\n", ans);
 	} else {
-		for(lo = 0; lo < CAP; lo ++) {
-			int index = Ind[lo];
-			ans += A[index];
+		for(i = 0; i < N; i ++) {
+			int index = Ind[i];
+			int load = A[index];
+			ans += do_work(load);
 		}
 		printf("ans = %d\n", ans);
 	}
@@ -52,13 +57,4 @@ int main(int argc, char **argv)
 	printf("Time = %f\n", (end.tv_sec -start.tv_sec) + 
 		(double) (end.tv_nsec - start.tv_nsec) / 1000000000);
 	
-	
-	clock_gettime(CLOCK_REALTIME, &start);
-	i = 2;
-	for(lo = 0; lo < CAP; lo ++) {
-		i %= 1000000;
-	}
-	clock_gettime(CLOCK_REALTIME, &end);
-	printf("Time = %f\n", (end.tv_sec -start.tv_sec) + 
-		(double) (end.tv_nsec - start.tv_nsec) / 1000000000);
 }
