78f1466f6194624cee98a30c00e5aa1cbc40d07a
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-04-21 01:45:10 -0400

    Nananana

--- sleep/usleep.c
+++ sleep/usleep.c
@@ -4,6 +4,9 @@
 /*
  * Measurements on xia-router0: Xeon L5640 @2.27 GHz
  * 100000 usleep(1) = 5.6 sec ==> usleep(1) = 56 us
+ * 
+ * Measurements on xia-router2: Xeon E5-2680 @2.7 GHz
+ * usleep(1) = 58 us
  */
 int main(int argc, char *argv[]) 
 {
