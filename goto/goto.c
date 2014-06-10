227958d1ce314bf16299a482145f00c94cc6c468
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-06-10 17:05:29 -0400

    Goto statement considered useful

---
+++ goto/goto.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+
+#define G_2_ ((2 * 1024 * 1024 * 1024) - 1)
+int sum = 0;
+int *ht_log;
+
+#define LOG_CAP (128 * 1024 * 1024)
+#define LOG_CAP_ ((128 * 1024 * 1024) - 1)
+#define LOG_SID 1
+
+int *pkts;
+#define NUM_PKTS (1024 * 1024)
+
+#define BATCH_SIZE 8
+#define BATCH_SIZE_ 7
+
+int batch_index = 0;
+int mem_addr[BATCH_SIZE];
+
+// Some compute function
+int hash(int a)
+{
+	int ret = a;
+	int i;
+	for(i = 0; i < 50; i++) {
+		ret = ret + rand();
+	}
+
+	return ret;
+}
+
+// Process BATCH_SIZE pkts starting from lo
+int process_pkts_in_range(int lo)
+{
+	batch_index = lo & BATCH_SIZE_;
+	
+	// Compute
+	mem_addr[batch_index] = hash(pkts[batch_index]) & LOG_CAP_;
+
+	// Memory access
+	sum += ht_log[mem_addr[batch_index]];
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	// Allocate a large memory area
+	int sid = shmget(LOG_SID, LOG_CAP * sizeof(int), IPC_CREAT | 0666 | SHM_HUGETLB);
+	if(sid < 0) {
+		fprintf(stderr, "Could not create ht_log\n");
+		exit(-1);
+	}
+	ht_log = shmat(sid, 0, 0);
+	for(i = 0; i < LOG_CAP; i ++) {
+		ht_log[i] = i;
+	}
+
+	// Allocate the packets
+	pkts = (int *) malloc(NUM_PKTS * sizeof(int));
+	for(i = 0; i < NUM_PKTS; i++) {
+		pkts[i] = rand() & LOG_CAP_;
+	}
+
+	printf("Finished creating ht_log and packets\n");
+
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	for(i = 0; i < NUM_PKTS; i += BATCH_SIZE) {
+		process_pkts_in_range(i);
+	}
+
+	clock_gettime(CLOCK_REALTIME, &end);
+	printf("Time = %f, sum = %d\n", 
+		(end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / 1000000000,
+		sum);
+}
