7544c3bdf029c42693492ba3da50ef8efff5f7b4
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-06-11 19:08:42 -0400

    Infinite loop with rip saving

---
+++ goto/rip-simple/goto.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<pthread.h>
+#include<time.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+
+#include "param.h"
+
+// Each packet contains a random integer. The memory address accessed
+// by the packet is determined by an expensive hash of the integer.
+
+int sum = 0;
+
+int *ht_log;
+#define LOG_CAP (128 * 1024 * 1024)
+#define LOG_CAP_ ((128 * 1024 * 1024) - 1)
+#define LOG_SID 1
+
+int *pkts;
+#define NUM_PKTS (16 * 1024 * 1024)
+
+#define BATCH_SIZE 8
+#define BATCH_SIZE_ 7
+
+int batch_index = 0;
+
+// Some compute function
+// Increment 'a' by at most COMPUTE * 4: the return value is still random
+int hash(int a)
+{
+	int ret = a;
+	int i;
+	for(i = 0; i < COMPUTE; i++) {
+		ret = ret + ((i * ret) % 5);
+	}
+
+	return ret;
+}
+
+// Process BATCH_SIZE pkts starting from lo
+int process_pkts_in_batch(int *pkt_lo)
+{
+	uint64_t my_rip;
+	asm volatile ("lea (%%rip), %0" : "=r"(my_rip));
+
+	asm volatile ("mov %0, %%rax" :: "r" (my_rip) : "rax");
+	asm volatile ("jmp *%rax");
+
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	// Allocate a large memory area
+	int sid = shmget(LOG_SID, LOG_CAP * sizeof(int), IPC_CREAT | 0666 | SHM_HUGETLB);
+	if(sid < 0) {
+		fprintf(stderr, "Could not create ht_log\n");
+		exit(-1);
+	}
+	ht_log = shmat(sid, 0, 0);
+	for(i = 0; i < LOG_CAP; i ++) {
+		ht_log[i] = i;
+	}
+
+	// Allocate the packets
+	pkts = (int *) malloc(NUM_PKTS * sizeof(int));
+	for(i = 0; i < NUM_PKTS; i++) {
+		pkts[i] = rand() & LOG_CAP_;
+	}
+
+	fprintf(stderr, "Finished creating ht_log and packets\n");
+
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	for(i = 0; i < NUM_PKTS; i += BATCH_SIZE) {
+		process_pkts_in_batch(&pkts[i]);
+	}
+
+	clock_gettime(CLOCK_REALTIME, &end);
+	printf("Time = %f sum = %d\n", 
+		(end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / 1000000000,
+		sum);
+}
