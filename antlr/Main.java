b613b88000e7adae0a5f96ad9a156ca2a8e4de48
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-06-28 02:39:28 -0400

    Add code to print local variables

--- antlr/Main.java
+++ antlr/Main.java
@@ -1,23 +1,51 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 class ExtractInterfaceListener extends CBaseListener {
 	CParser parser;
+	TokenStream tokens;
 	public ExtractInterfaceListener(CParser parser) {
 		this.parser = parser;
+		tokens = parser.getTokenStream();
 	}
+	
 	
 	/* Listen to matches of method declaration */
 	@Override
 	public void enterFunctionDefinition(CParser.FunctionDefinitionContext ctx) {
-		System.out.println(ctx.getText());
+	//	printContext(ctx);
+	}
+	
+	/* Listen to variable defns */
+	@Override
+	public void enterInitDeclarator(CParser.InitDeclaratorContext ctx) {	
+		System.out.println(ctx.declarator().getText());
+	}
+
+	@Override
+	public void enterEveryRule(@NotNull ParserRuleContext ctx) {
+		//printContext(ctx);
+	}
+
+	private void printContext(ParserRuleContext ctx) {
+		int startIndex = ctx.getStart().getTokenIndex();
+		int stopIndex = ctx.getStop().getTokenIndex();
+
+		for(int i = startIndex; i <= stopIndex; i ++) {
+			System.out.print(tokens.get(i).getText() + " ");	
+		}
+		
+		System.out.println("\n");
 	}
 }
 
@@ -31,8 +59,10 @@ public class Main {
 		CLexer lexer = new CLexer(charStream);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		CParser parser = new CParser(tokens);
-		ParserRuleContext tree = parser.compilationUnit();
 		
+		// Parse and get the root of the parse tree
+		ParserRuleContext tree = parser.compilationUnit();
+
 		ParseTreeWalker walker = new ParseTreeWalker();
 		ExtractInterfaceListener extractor = new ExtractInterfaceListener(parser);
 		walker.walk(extractor, tree);
