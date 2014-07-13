d03f257bc680f4af7eb6610bf82c86983c2c908b
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-07-13 14:16:53 -0400

    Create a simple example for tcm

--- goto/locals/goto.c
+++ goto/locals/goto.c
@@ -41,26 +41,7 @@ int process_pkts_in_batch(int *pkt_lo)
 {
 	int I = 0;
 	void *batch_rips[BATCH_SIZE];
-	int a_1[BATCH_SIZE];
-	int a_2[BATCH_SIZE];
-	int a_3[BATCH_SIZE];
-	int a_4[BATCH_SIZE];
-	int a_5[BATCH_SIZE];
-	int a_6[BATCH_SIZE];
-	int a_7[BATCH_SIZE];
-	int a_8[BATCH_SIZE];
-	int a_9[BATCH_SIZE];
-	int a_10[BATCH_SIZE];
-	int a_11[BATCH_SIZE];
-	int a_12[BATCH_SIZE];
-	int a_13[BATCH_SIZE];
-	int a_14[BATCH_SIZE];
-	int a_15[BATCH_SIZE];
-	int a_16[BATCH_SIZE];
-	int a_17[BATCH_SIZE];
-	int a_18[BATCH_SIZE];
-	int a_19[BATCH_SIZE];
-	int a_20[BATCH_SIZE];
+	int a1[BATCH_SIZE];
 
 	int temp_index;
 	for(temp_index = 0; temp_index < BATCH_SIZE; temp_index ++) {
@@ -69,28 +50,9 @@ int process_pkts_in_batch(int *pkt_lo)
 
 label_0:
 	
-	a_1[I] = hash(pkt_lo[I]) & LOG_CAP_;
-	a_2[I] = hash(a_1[I]) & LOG_CAP_;
-	a_3[I] = hash(a_2[I]) & LOG_CAP_;
-	a_4[I] = hash(a_3[I]) & LOG_CAP_;
-	a_5[I] = hash(a_4[I]) & LOG_CAP_;
-	a_6[I] = hash(a_5[I]) & LOG_CAP_;
-	a_7[I] = hash(a_6[I]) & LOG_CAP_;
-	a_8[I] = hash(a_7[I]) & LOG_CAP_;
-	a_9[I] = hash(a_8[I]) & LOG_CAP_;
-	a_10[I] = hash(a_9[I]) & LOG_CAP_;
-	a_11[I] = hash(a_10[I]) & LOG_CAP_;
-	a_12[I] = hash(a_11[I]) & LOG_CAP_;
-	a_13[I] = hash(a_12[I]) & LOG_CAP_;
-	a_14[I] = hash(a_13[I]) & LOG_CAP_;
-	a_15[I] = hash(a_14[I]) & LOG_CAP_;
-	a_16[I] = hash(a_15[I]) & LOG_CAP_;
-	a_17[I] = hash(a_16[I]) & LOG_CAP_;
-	a_18[I] = hash(a_17[I]) & LOG_CAP_;
-	a_19[I] = hash(a_18[I]) & LOG_CAP_;
-	a_20[I] = hash(a_19[I]) & LOG_CAP_;
-	
-	__builtin_prefetch(&ht_log[a_10[I]], 0, 0);
+	a1[I] = hash(pkt_lo[I]) & LOG_CAP_;
+
+	__builtin_prefetch(&ht_log[a1[I]], 0, 0);
 	batch_rips[I] = &&label_1;
 
 	I = (I + 1) & BATCH_SIZE_;
@@ -99,7 +61,7 @@ label_0:
 	}
 
 label_1:
-	sum += ht_log[a_10[I]];
+	sum += ht_log[a1[I]];
 	I = (I + 1) & BATCH_SIZE_;
 	if(I != 0) {
 		goto *batch_rips[I];
