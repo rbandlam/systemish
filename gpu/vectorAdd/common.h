e4e570cbf125a72681e54fba0d1e5e47b69f878a
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-07-20 05:03:25 -0400

    More lint

--- gpu/vectorAdd/common.h
+++ gpu/vectorAdd/common.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <time.h>
 
 void printDeviceProperties();
 
--- gpu/vectorAdd/vectorAdd.cu
+++ gpu/vectorAdd/vectorAdd.cu
@@ -11,100 +11,97 @@ vectorAdd(const float *A, const float *B, float *C, int N)
     }
 }
 
-int
-main(void)
+int main(void)
 {
 	int err, N = 500000;
 	printDeviceProperties();
 
-    printf("[Vector addition of %d elements]\n", N);
+	printf("[Vector addition of %d elements]\n", N);
 
 	// Allocate host vectors
-    float *h_A = (float *) malloc(N * sizeof(float));
-    float *h_B = (float *) malloc(N * sizeof(float));
-    float *h_C = (float *) malloc(N * sizeof(float));
+	float *h_A = (float *) malloc(N * sizeof(float));
+	float *h_B = (float *) malloc(N * sizeof(float));
+	float *h_C = (float *) malloc(N * sizeof(float));
 
-    // Verify that allocations succeeded
-    if (h_A == NULL || h_B == NULL || h_C == NULL)
-    {
-        fprintf(stderr, "Failed to allocate host vectors!\n");
-        exit(EXIT_FAILURE);
-    }
+	// Verify that allocations succeeded
+	if (h_A == NULL || h_B == NULL || h_C == NULL) {
+		fprintf(stderr, "Failed to allocate host vectors!\n");
+		exit(EXIT_FAILURE);
+	}
 
-    // Initialize the host input vectors
-    for (int i = 0; i < N; ++i)
-    {
-        h_A[i] = rand()/(float)RAND_MAX;
-        h_B[i] = rand()/(float)RAND_MAX;
-    }
+	// Initialize the host input vectors
+	for (int i = 0; i < N; ++i)	{
+		h_A[i] = rand() / (float) RAND_MAX;
+		h_B[i] = rand() / (float)RAND_MAX;
+	}
 
-    // Allocate the device input vector A
-    float *d_A = NULL, *d_B = NULL, *d_C = NULL;
+	// Allocate the device input vector A
+	float *d_A = NULL, *d_B = NULL, *d_C = NULL;
 
-    err = cudaMalloc((void **)&d_A, N * sizeof(float));
+	err = cudaMalloc((void **)&d_A, N * sizeof(float));
 	CPE(err != cudaSuccess, "Failed to allocate d_A\n", -1);
 
-    err = cudaMalloc((void **)&d_B, N * sizeof(float));
+	err = cudaMalloc((void **)&d_B, N * sizeof(float));
 	CPE(err != cudaSuccess, "Failed to allocate d_B\n", -1);
 
-    err = cudaMalloc((void **)&d_C, N * sizeof(float));
+	err = cudaMalloc((void **)&d_C, N * sizeof(float));
 	CPE(err != cudaSuccess, "Failed to allocate d_C\n", -1);
 
 	// Copy A and B to device memory
-    printf("Copy input data from the host memory to the CUDA device\n");
+	printf("Copy input data from the host memory to the CUDA device\n");
 
-    err = cudaMemcpy(d_A, h_A, N * sizeof(float), cudaMemcpyHostToDevice);
+	err = cudaMemcpy(d_A, h_A, N * sizeof(float), cudaMemcpyHostToDevice);
 	CPE(err != cudaSuccess, "Failed to copy d_A to device memory\n", -1);
 
-    err = cudaMemcpy(d_B, h_B, N * sizeof(float), cudaMemcpyHostToDevice);
+	err = cudaMemcpy(d_B, h_B, N * sizeof(float), cudaMemcpyHostToDevice);
 	CPE(err != cudaSuccess, "Failed to copy d_B to device memory\n", -1);
 
-    // Launch the Vector Add CUDA Kernel
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+	// Launch the Vector Add CUDA Kernel
+	int threadsPerBlock = 256;
+	int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+	printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
 
-    err = cudaGetLastError();
+
+	vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+	err = cudaGetLastError();
 	CPE(err != cudaSuccess, "Failed to launch vectorAdd kernel\n", -1);
 
-    // Copy the device result vector in device memory to the host result vector
-    // in host memory.
-    printf("Copy output data from the CUDA device to the host memory\n");
-    err = cudaMemcpy(h_C, d_C, N * sizeof(float), cudaMemcpyDeviceToHost);
+	// Copy the device result vector in device memory to the host result vector
+	// in host memory.
+	printf("Copy output data from the CUDA device to the host memory\n");
+	err = cudaMemcpy(h_C, d_C, N * sizeof(float), cudaMemcpyDeviceToHost);
 	CPE(err != cudaSuccess, "Failed to copy C from device to host\n", -1);
 
-    // Verify that the result vector is correct
-    for (int i = 0; i < N; ++i)
-    {
-        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5)
-        {
-            fprintf(stderr, "Result verification failed at element %d!\n", i);
-            exit(EXIT_FAILURE);
-        }
-    }
-    printf("Test PASSED\n");
+	// Verify that the result vector is correct
+	for (int i = 0; i < N; ++i) {
+		if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
+			fprintf(stderr, "Result verification failed at element %d!\n", i);
+			exit(EXIT_FAILURE);
+		}
+	}
+	printf("Test PASSED\n");
 
-    // Free device global memory
-    err = cudaFree(d_A);
+	// Free device global memory
+	err = cudaFree(d_A);
 	CPE(err != cudaSuccess, "Failed to free d_A\n", -1);
 
-    err = cudaFree(d_B);
+	err = cudaFree(d_B);
 	CPE(err != cudaSuccess, "Failed to free d_B\n", -1);
 
-    err = cudaFree(d_C);
+	err = cudaFree(d_C);
 	CPE(err != cudaSuccess, "Failed to free d_C\n", -1);
 
-    // Free host memory
-    free(h_A);
-    free(h_B);
-    free(h_C);
+	// Free host memory
+	free(h_A);
+	free(h_B);
+	free(h_C);
 
-    // Reset the device and exit
-    err = cudaDeviceReset();
+	// Reset the device and exit
+	err = cudaDeviceReset();
 	CPE(err != cudaSuccess, "Failed to de-initialize the device\n", -1);
 
-    printf("Done\n");
-    return 0;
+	printf("Done\n");
+	return 0;
 }
 
