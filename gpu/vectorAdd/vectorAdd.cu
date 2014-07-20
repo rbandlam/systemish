ae037cede777d088c84528b91e4ba0272e7e5d37
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-07-20 03:25:26 -0400

    Can print GPU name

--- gpu/vectorAdd/vectorAdd.cu
+++ gpu/vectorAdd/vectorAdd.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include "common.h"
 
 __global__ void
 vectorAdd(const float *A, const float *B, float *C, int numElements)
@@ -15,6 +16,11 @@ vectorAdd(const float *A, const float *B, float *C, int numElements)
 int
 main(void)
 {
+	struct cudaDeviceProp deviceProp;
+	int ret = cudaGetDeviceProperties(&deviceProp, 0);
+	CPE(ret != cudaSuccess, "Get Device Properties failed\n", -1);
+	printf("Device name: %s\n", deviceProp.name);
+
     // Error code to check return values for CUDA calls
     cudaError_t err = cudaSuccess;
 
