68fc3b2e194701a0875e318e96f6eb739e735e1b
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-08-04 23:09:46 -0400

    Base unified memory commit. Does not work with multiple threads.

--- gpu/unifiedMemory/unifiedMemory.cu
+++ gpu/unifiedMemory/unifiedMemory.cu
@@ -1,8 +1,8 @@
 #include "common.h"
 
-#define NUM_THREADS 4
+#define NUM_THREADS 1
 
-int *h_A[NUM_THREADS];
+int *A[NUM_THREADS];
 int tid[NUM_THREADS];
 pthread_t thread[NUM_THREADS];
 
@@ -23,7 +23,6 @@ void *gpu_run(void *ptr)
 
 	cudaStream_t my_stream;
 		
-	int *d_A = NULL;
 	int err = cudaSuccess;
 
 	// Per stage measurements
@@ -42,44 +41,34 @@ void *gpu_run(void *ptr)
 	err = cudaStreamCreate(&my_stream);
 	CPE(err != cudaSuccess, "Failed to create cudaStream\n", -1);
 
-	err = cudaMalloc((void **) &d_A, NUM_PKTS * sizeof(int));
-	CPE(err != cudaSuccess, "Failed to cudaMalloc\n", -1);
-
-	// Measure host-to-device memcpy latency
 	for(i = 0; i < ITERS; i ++) {
 		start_cycles = get_cycles();
 		
-		// Initialize h_A
-		for(j = 0; j < NUM_PKTS; j ++) {
-			h_A[tid][j] = (i & 0xff) + j;
-		}
-
-		// Stage 1: host to device memcpy
+		// Stage 1: host to device latency
 		start_cycles_h2d = get_cycles();
-		cudaMemcpyAsync(d_A, h_A[tid], NUM_PKTS * sizeof(int), cudaMemcpyHostToDevice, my_stream);
+		for(j = 0; j < NUM_PKTS; j ++) {
+			A[tid][j] = (i & 0xff) + j;
+		}
 		end_cycles_h2d = get_cycles();
 
-		// Stage 2: kernel execution
+		// Stage 2: kernel execution latency
 		start_cycles_kernel = get_cycles();
-		vectorAdd<<<blocksPerGrid, threadsPerBlock, 0, my_stream>>>(d_A, NUM_PKTS);
+		vectorAdd<<<blocksPerGrid, threadsPerBlock, 0, my_stream>>>(A[tid], NUM_PKTS);
 		end_cycles_kernel = get_cycles();
-
-		// Stage 2: device to host memcpy
-		start_cycles_d2h = get_cycles();
-		cudaMemcpyAsync(h_A[tid], d_A, NUM_PKTS * sizeof(int), cudaMemcpyDeviceToHost, my_stream);
-		end_cycles_d2h = get_cycles();
 
 		// Complete full execution: the time for this is not included in per-stage measurement
 		cudaStreamSynchronize(my_stream);
 
-		// Verify kernel's result
+		// Stage 2: device to host latency
+		start_cycles_d2h = get_cycles();
 		for(j = 0; j < NUM_PKTS; j ++) {
 			int kernel_inp = (i & 0xff) + j;
-			if(h_A[tid][j] != kernel_inp * kernel_inp) {
+			if(A[tid][j] != kernel_inp * kernel_inp) {
 				fprintf(stderr, "Kernel output mismatch error\n");
 				exit(-1);
 			}
 		}
+		end_cycles_d2h = get_cycles();
 
 		end_cycles = get_cycles();
 
@@ -101,8 +90,6 @@ void *gpu_run(void *ptr)
 		err = cudaGetLastError();
 		CPE(err != cudaSuccess, "Fail!\n", -1);
 	}
-
-	cudaFree(d_A);
 
 	printf("\nFull execution stats:\n");
 	printf("\tThread %d: h2d = %d ns, kernel = %d ns, d2h = %d ns, full execution = %d ns\n",
@@ -126,18 +113,14 @@ int main(void)
 
 	// Allocate host vectors in pinned memory
 	for(int thread_i = 0; thread_i < NUM_THREADS; thread_i ++) {
-		err = cudaMallocHost((void **) &h_A[thread_i], NUM_PKTS * sizeof(int));
-		CPE(err != cudaSuccess, "Could not allocate pinned memory\n", -1);
+		err = cudaMallocManaged(&A[thread_i], NUM_PKTS * sizeof(int));
+		CPE(err != cudaSuccess, "Could not allocate managed memory\n", -1);
 
-		// Verify that allocations succeeded
-		if (h_A[thread_i] == NULL) {
-			fprintf(stderr, "Failed to allocate host vectors!\n");
-			exit(EXIT_FAILURE);
-		}
+		assert(A[thread_i] != NULL);
 
-		// Initialize the host input vectors
+		// Initialize the managed memory vectors
 		for(int j = 0; j < NUM_PKTS; j++)	{
-			h_A[thread_i][j] = thread_i + j;
+			A[thread_i][j] = thread_i + j;
 		}
 	}
 
@@ -150,9 +133,9 @@ int main(void)
 		pthread_join(thread[thread_i], NULL);
 	}	
 
-	// Free pinned host memory
+	// Free allocated managed memory
 	for(int thread_i = 0; thread_i < NUM_THREADS; thread_i ++) {
-		cudaFreeHost(h_A[thread_i]);
+		cudaFree(A[thread_i]);
 	}
 
 	// Reset the device and exit
