b28bc37de953cdbe4bd1e6481c574ebf6916d89b
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-07 04:22:14 -0400

    Modify random memory benchmark to compare between CPU and GPU. Add knobs for using hugepages and interleaving.

--- scripts/dpdk/xia-router0-init.sh
+++ scripts/dpdk/xia-router0-init.sh
@@ -1,4 +1,10 @@
-# Script to initialize DPDK on APT. Run as root.
+# Script to initialize DPDK on xia-router0. Run as root.
+
+function blue() {
+	es=`tput setaf 4`
+	ee=`tput sgr0`
+	echo "${es}$1${ee}"
+}
 
 ESC_SEQ="\x1b["
 COL_RED=$ESC_SEQ"31;01m"
@@ -8,43 +14,42 @@ RTE_SDK=/home/akalia/dpdk-1.5.0r0/
 RTE_TARGET=x86_64-default-linuxapp-gcc
 
 # Increase shmmax and shmall parameters
-echo -e "$COL_RED Increasing shmmax and shmall $COL_RESET"
-sudo sysctl -w kernel.shmmax=2147483648     # Bytes
-sudo sysctl -w kernel.shmall=2147483648     # Pages
+blue "Increasing shmmax and shmall"
+kernel.shmmax = 9223372036854775807
+kernel.shmall = 1152921504606846720
 sudo sysctl -p /etc/sysctl.conf
 
 # Create hugepage mount
-echo -e "$COL_RED Creating /mnt/huge $COL_RESET"
+blue "Creating /mnt/huge"
 umount /mnt/huge
 rm -rf /mnt/huge
 mkdir /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
 
 # Install uio modules
-echo -e "$COL_RED Installing uio and igb_uio modules $COL_RESET"
+blue "Installing uio and igb_uio modules"
 modprobe uio
-rmmod igb_uio
 insmod $RTE_SDK/$RTE_TARGET/kmod/igb_uio.ko
 
 # Create some hugepages
-echo -e "$COL_RED Creating 2048 hugepages on both sockets $COL_RESET"
+blue "Creating 2048 hugepages on both sockets"
 echo 2048 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 echo 2048 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
-echo -e "$COL_RED Done creating hugepages. Status: $COL_RESET"
+blue "Done creating hugepages. Status:"
 cat /sys/devices/system/node/*/meminfo | grep Huge
 
 # Bind 10 GbE ports to igb_uio
-echo -e "$COL_RED Bringing xge0 through xge3 down: $COL_RESET"
+blue "Bringing xge0 through xge3 down:"
 for i in `seq 0 3`; do
 	ifconfig xge$i down
 done
 
-echo -e "$COL_RED Binding ports to igb_uio: $COL_RESET"
+blue "Binding ports to igb_uio:"
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 02:00.0
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 02:00.1
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 08:00.0
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 08:00.1
-echo -e "$COL_RED Done binding ixgbe to igb_uio. Status: $COL_RESET"
+blue "Done binding ixgbe to igb_uio. Status:"
 
 $RTE_SDK/tools/pci_unbind.py --status
 
--- scripts/dpdk/xia-router2-init.sh
+++ scripts/dpdk/xia-router2-init.sh
@@ -1,4 +1,10 @@
-# Script to initialize DPDK on APT. Run as root.
+# Script to initialize DPDK on xia-router2. Run as root.
+
+function blue() {
+	es=`tput setaf 4`
+	ee=`tput sgr0`
+	echo "${es}$1${ee}"
+}
 
 ESC_SEQ="\x1b["
 COL_RED=$ESC_SEQ"31;01m"
@@ -8,46 +14,46 @@ RTE_SDK=/home/akalia/dpdk-1.5.0r0/
 RTE_TARGET=x86_64-default-linuxapp-gcc
 
 # Increase shmmax and shmall parameters
-echo -e "$COL_RED Increasing shmmax and shmall $COL_RESET"
+blue "Increasing shmmax and shmall"
 kernel.shmmax = 9223372036854775807
 kernel.shmall = 1152921504606846720
 sudo sysctl -p /etc/sysctl.conf
 
 # Create hugepage mount
-echo -e "$COL_RED Creating /mnt/huge $COL_RESET"
+blue "Creating /mnt/huge"
 umount /mnt/huge
 rm -rf /mnt/huge
 mkdir /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
 
 # Install uio modules
-echo -e "$COL_RED Installing uio and igb_uio modules $COL_RESET"
+blue "Installing uio and igb_uio modules"
 modprobe uio
 insmod $RTE_SDK/$RTE_TARGET/kmod/igb_uio.ko
 
 # Create some hugepages
-echo -e "$COL_RED Creating 2048 hugepages on both sockets $COL_RESET"
-#echo 2048 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-echo 4192 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
-echo -e "$COL_RED Done creating hugepages. Status: $COL_RESET"
+blue "Creating 4192 hugepages on both sockets"
+echo 4192 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+#echo 4192 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+blue "Done creating hugepages. Status:"
 cat /sys/devices/system/node/*/meminfo | grep Huge
 
 # Bind 10 GbE ports to igb_uio
-echo -e "$COL_RED Bringing xge0 through xge7 down: $COL_RESET"
+blue "Bringing xge0 through xge7 down:"
 for i in `seq 0 7`; do
 	ifconfig xge$i down
 done
 
-echo -e "$COL_RED Binding ports to igb_uio: $COL_RESET"
-$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 02:00.0
-$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 02:00.1
-$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 05:00.0
-$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 05:00.1
+blue "Binding ports to igb_uio:"
+$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 01:00.0
+$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 01:00.1
+$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 03:00.0
+$RTE_SDK/tools/pci_unbind.py --bind=igb_uio 03:00.1
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 42:00.0
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 42:00.1
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 43:00.0
 $RTE_SDK/tools/pci_unbind.py --bind=igb_uio 43:00.1
-echo -e "$COL_RED Done binding ixgbe to igb_uio. Status: $COL_RESET"
+blue "Done binding ixgbe to igb_uio. Status:"
 
 $RTE_SDK/tools/pci_unbind.py --status
 
