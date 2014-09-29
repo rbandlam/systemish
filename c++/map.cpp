c64b7eff28d84c547ef2d4562a0971f80a2cfbac
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-29 02:44:51 -0400

    Add a C++ map example

---
+++ c++/map.cpp
@@ -0,0 +1,28 @@
+/** < Map pairs to structs */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <map>
+using namespace std;
+
+struct stats {
+	long long tot_ref;
+	long long tot_misses;
+};
+
+int main()
+{
+	std::map<std::pair<int, int>, struct stats> profile;
+
+	struct stats st_1;
+	st_1.tot_ref = 1;
+	st_1.tot_misses = 3185;
+
+	profile.insert(std::make_pair(std::make_pair(32, 0), st_1));
+
+	struct stats out = profile[std::make_pair(32, 0)];
+	printf("%lld %lld\n", out.tot_ref, out.tot_misses);
+
+	out = profile[std::make_pair(31, 0)];
+	printf("%lld %lld\n", out.tot_ref, out.tot_misses);
+}
